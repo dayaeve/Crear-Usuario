97bc9b886981f854279cdeeb638e1c1c0e252c00
dayaeve <dayanavillacis07@gmail.com>
2023-12-30 19:45:27 -0500

    advances

--- src/hooks/useCrud.js
+++ src/hooks/useCrud.js
@@ -6,14 +6,14 @@ const useCrud = (baseUrl) => {
 
     //GET
     const getApi = (path) => {
-        const url = `${baseUrl}${path}/`
+        const url = `${baseUrl}${path}`
         axios.get(url)
         .then(res => setInfoApi(res.data))
         .catch(err => console.log(err))
     }
     //POST
     const postApi = (path, data) => {
-        const url = `${baseUrl}${path}/`
+        const url = `${baseUrl}${path}`
         axios.post(url, data)
         .then(res => {
             console.log(res.data)
@@ -23,7 +23,7 @@ const useCrud = (baseUrl) => {
     }
     //DELETE
     const deleteApi = (path, id) => {
-        const url = `${baseUrl}${path}/${id}/`
+        const url = `${baseUrl}${path}/${id}`
         axios.delete(url)
         .then( res => {
             setInfoApi(infoApi.filter(e => e.id !== id))
@@ -32,7 +32,7 @@ const useCrud = (baseUrl) => {
     }
     //UPDATE
     const updateApi = (path, id, data) => {
-        const url = `${baseUrl}${path}/${id}/`
+        const url = `${baseUrl}${path}/${id}`
         axios.patch(url, data)
         .then( res => {
             console.log(res.data);
